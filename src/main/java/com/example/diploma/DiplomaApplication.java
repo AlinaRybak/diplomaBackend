9a30fd7fa0d35c70acc8a3f6e38479c1ab54b68b
Alina <alinarybak88@gmail.com>
2023-07-18 16:27:00 +0200

    heroku

--- src/main/java/com/example/diploma/DiplomaApplication.java
+++ src/main/java/com/example/diploma/DiplomaApplication.java
@@ -1,19 +1,10 @@
 package com.example.diploma;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class DiplomaApplication {
     public static void main(String[] args) {
         SpringApplication.run(DiplomaApplication.class, args);
-    }
-
-    @GetMapping("user")
-    public String getUser(){
-        return "Hello";
     }
 }
