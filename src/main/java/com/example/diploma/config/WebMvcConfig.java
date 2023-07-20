da6908a8257734ce7690855b14dd9cad43fd231c
Alina <alinarybak88@gmail.com>
2023-07-20 10:03:35 +0200

    cors

---
+++ src/main/java/com/example/diploma/config/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package com.example.diploma.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://green-food-brands.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Origin", "X-Requested-With", "Content-Type", "Accept");
+    }
+}
