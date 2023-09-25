99f7228735d103cc6cc357272ea551233758b89f
Alina <alinarybak88@gmail.com>
2023-09-25 11:44:54 +0200

    correct user

--- src/main/java/com/example/diploma/service/NewCompanyService.java
+++ src/main/java/com/example/diploma/service/NewCompanyService.java
@@ -1,10 +1,8 @@
 package com.example.diploma.service;
-
 import com.example.diploma.model.NewCompany;
 import com.example.diploma.repository.NewCompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
--- src/main/java/com/example/diploma/service/UserService.java
+++ src/main/java/com/example/diploma/service/UserService.java
@@ -3,7 +3,6 @@ import com.example.diploma.model.User;
 import com.example.diploma.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -16,9 +15,12 @@ import java.util.Optional;
             this.userRepository = userRepository;
         }
 
-        public User save(User user) {
-            return userRepository.save(user);
+    public User save(User user) {
+        if (user.getPhoneNumber() == null) {
+            user.setPhoneNumber("DEFAULT_PHONE_NUMBER");
         }
+        return userRepository.save(user);
+    }
 
     public List<User> getUsersInfo() {
         List<User> userInfoList = userRepository.findAll();
